61d5c8bcd49ddaaf4c774869277d9f1e5429c877
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-01-09 12:00:37 +0100

    first push

--- src/page/AddTask.tsx
+++ src/page/AddTask.tsx
@@ -4,7 +4,7 @@ export const AddTask = () => {
       <div className="w-full h-screen flex flex-col justify-center items-center">
         <input className="border p-2" type="text" placeholder="Enter task..." />
         <button className="bg-sky-600 text-white px-5 py-2 my-10  ">Add</button>
-        <div className="w-[400px] bg-red-500 h-[40px] text-white p-5 flex items-center  rounded-md">
+        <div className="w-[400px] bg-red-500 h-[40px] text-white p-5 flex items-center rounded-md">
           hejejej
         </div>
       </div>
